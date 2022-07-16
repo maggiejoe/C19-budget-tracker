e1040ea4073fe9684e699bb1a40a739ef3a80801
maggiejoe <maggiejoseph94@gmail.com>
2022-07-16 16:54:41 -0400

    add mongodb to deploy on heroku

--- server.js
+++ server.js
@@ -16,11 +16,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  // useFindAndModify: false,
-  useUnifiedTopology: true
-});
+mongoose.connect(MONGODB_URI || "mongodb://localhost/budget",
+  {
+    useNewUrlParser: true,
+    // useFindAndModify: false,
+    useUnifiedTopology: true
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
